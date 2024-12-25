a238ad969862ea3ab7ec38baab93a9ae4ab281dc
chandra <chandratripmp@gmail.com>
2024-12-25 23:59:07 +0700

    add dependencies config

---
+++ config/dependencies.php
@@ -0,0 +1,17 @@
+<?php
+
+use DI\ContainerBuilder;
+use Services\LoanService;
+use Controllers\LoanController;
+use Validators\LoanValidator;
+
+$containerBuilder = new ContainerBuilder();
+
+$containerBuilder->addDefinitions([
+    // Controller configurations
+    LoanController::class => function (LoanService $service) {
+        return new LoanController($service, new LoanValidator());
+    },
+]);
+
+return $containerBuilder->build();
